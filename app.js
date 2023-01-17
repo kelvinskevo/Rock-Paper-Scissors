a421d6335d2b8269bb44c76626fd79edab78194e
Kelvin <kelvinchemiati@gmail.com>
2023-01-17 14:35:35 +0300

    update

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const computerChoiceDisplay = document.getElementById("computers-choice");
 const usersChoiceDisplay = document.getElementById("users-choice");
-const result = document.getElementById("results");
+const resultDisplay = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
 let usersChoice;
 let computerChoice;
+let results;
 
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener("click", (e) => {
     usersChoice = e.target.id;
     usersChoiceDisplay.innerHTML = usersChoice;
-    generateComputerChoice();
+    getComputerChoice();
+    getresult();
   })
 );
 
-function generateComputerChoice() {
-  const randomNumber = Math.random() * 3;
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
 
   if (randomNumber === 1) {
     computerChoice = "rock";
@@ -24,4 +26,19 @@ function generateComputerChoice() {
     computerChoice = "scissors";
   }
   computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getresult() {
+  if (usersChoice === computerChoice) {
+    results = "Its a Tie";
+  } else if (usersChoice === "rock" && computerChoice === "scissors") {
+    results = "You won";
+  } else if (usersChoice === "scissors" && computerChoice === "paper") {
+    results = "You won";
+  } else if (usersChoice === "paper" && computerChoice === "rock") {
+    results = "You won";
+  } else {
+    results = "You lost";
+  }
+  resultDisplay.innerHTML = results;
 }
